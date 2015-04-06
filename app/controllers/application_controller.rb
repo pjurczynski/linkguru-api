bfbc860821e3ed448a9242de1d970764ad98976d
Rafał Gawlik <rafal.maly@gmail.com>
2015-04-06 17:06:34 +0200

    serializers setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
+class SerializserScope
+  attr_reader :current_user
+
+  def initialize(current_user)
+    @current_user = current_user
+  end
+end
+
 class ApplicationController < ActionController::API
+  include ActionController::Serialization
+  include ActionController::MimeResponds
+  include ActionController::ImplicitRender
+
+  serialization_scope :serializer_scope
+
+  respond_to :json
+
+  def default_serializer_options
+    { root: false }
+  end
+
+  protected
+
+  def serializer_scope
+    SerializserScope.new(current_user)
+  end
 end
