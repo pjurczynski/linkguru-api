dcf35d3b50c9ec4a53d7812c22be956493fbf5b2
Rafał Gawlik <rafal.maly@gmail.com>
2015-04-06 17:30:40 +0200

    link serializesr

--- app/serializers/link_serializer.rb
+++ app/serializers/link_serializer.rb
@@ -1,3 +1,24 @@
 class LinkSerializer < ActiveModel::Serializer
-  attributes :id, :description, :url
+  attributes :id, :description, :url, :tag_list, :score, :upVoted, :downVoted, :owned
+  delegate :current_user, to: :scope
+
+  has_one :user, serializer: UserSerializer, key: :owner
+
+  def owned
+    object.user == current_user
+  end
+
+  def score
+    object.weighted_score
+  end
+
+  def upVoted
+    return true if owned
+    object.votes_for(current_user).first.try(:vote_flag) == true
+  end
+
+  def downVoted
+    return true if owned
+    object.votes_for(current_user).first.try(:vote_flag) == false
+  end
 end
