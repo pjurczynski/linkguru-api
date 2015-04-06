b18214157888d04c33f597a3668cbd695c7da1b6
Rafał Gawlik <rafal.maly@gmail.com>
2015-04-06 17:30:55 +0200

    refactor links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,34 +1,21 @@
 class LinksController < ApplicationController
+  before_action :authenticate!
+
   def index
-    @links = requested_links(params[:timestamp])
-
-    render json: @links
-  end
-
-  def show
-    @link = Link.find(params[:id])
-
-    render json: @link
+    links = requested_links(params[:timestamp])
+    respond_with links, each_serializer: LinkSerializer
   end
 
   def create
-    @link = Link.new(link_params)
-
-    if @link.save
-      render json: @link, status: :created, location: @link
-    else
-      render json: @link.errors, status: :unprocessable_entity
-    end
+    link = Link.new(link_params)
+    link.user = current_user
+    link.save
+    respond_with link, serializer: LinkSerializer
   end
 
   def update
-    @link = Link.find(params[:id])
-
-    if @link.update(link_params)
-      head :no_content
-    else
-      render json: @link.errors, status: :unprocessable_entity
-    end
+    link.update(link_params)
+    respond_with link, serializer: LinkSerializer
   end
 
   def destroy
@@ -40,8 +27,13 @@ class LinksController < ApplicationController
 
   private
 
+
+  def link
+    link = Link.find(params[:link_id])
+  end
+
   def link_params
-    params.require(:link).permit(:description, :url)
+    params.require(:link).permit(:description, :url, tag_list: [])
   end
 
   def requested_links(timestamp_string)
