dd3f36e81b390818e6f8dbc64c30855a24d6ccb7
Radosław Piątek <radoslaw.piatek@netguru.pl>
2015-04-17 12:44:52 +0200

    Extract google authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,34 +1,8 @@
-require 'google/api_client'
-require 'google/api_client/client_secrets'
-
-PLUS_LOGIN_SCOPE = 'email'
-
-$credentials = Google::APIClient::ClientSecrets.load
-$authorization = Signet::OAuth2::Client.new(
-    :authorization_uri => $credentials.authorization_uri,
-    :token_credential_uri => $credentials.token_credential_uri,
-    :client_id => $credentials.client_id,
-    :client_secret => $credentials.client_secret,
-    :redirect_uri => $credentials.redirect_uris.first,
-    :access_type => 'offline',
-    :scope => PLUS_LOGIN_SCOPE)
-$client = Google::APIClient.new
-
-$allowed_domians = %w(gmail.com netguru.co netguru.pl)
-
 class SessionsController < ApplicationController
-
   def create
-    $authorization.code = params[:code]
-    $authorization.fetch_access_token!
-    $client.authorization = $authorization
-
-    plus = $client.discovered_api('plus', 'v1')
-    response = $client.execute!(plus.people.get, userId: 'me')
-    body = JSON.parse(response.body)
-    email = body['emails'].select { |x| x['type'] == 'account' }.first['value']
+    email = GoogleOauthAuthorizer.new.call
     domain = email.split('@').last
-    if $allowed_domians.include?(domain)
+    if allowed_domains.include?(domain)
       user = User.where(email: email).first_or_create(name: body['displayName'])
       user.generate_token!
       respond_with user, serializer: UserSerializer, location: false
@@ -39,6 +13,12 @@ class SessionsController < ApplicationController
 
   def destroy
     current_user.update_attribute(:token, nil)
-    respond_with json: {sucess: true}
+    respond_with json: { sucess: true }
+  end
+
+  private
+
+  def allowed_domains
+    %w(gmail.com netguru.co netguru.pl)
   end
 end
