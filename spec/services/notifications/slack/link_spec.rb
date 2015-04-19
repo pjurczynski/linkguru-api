f88faf925a85dedde915595f67ed188a99b21ce1
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-04-19 14:50:50 +0200

    Revert "disable tests for build"
    This reverts commit eefc0ad79d541a8007dabb3feef7762faf6080b5.

--- spec/services/notifications/slack/link_spec.rb
+++ spec/services/notifications/slack/link_spec.rb
@@ -2,5 +2,25 @@ require 'rails_helper'
 
 module Notifications::Slack
   describe Link do
+    let(:user) { build(:user) }
+    let(:link) { build(:link, user: user) }
+
+    subject { described_class.new(link) }
+
+    it 'slack responds with OK' do
+      expect(subject.call.code).to eq '200'
+    end
+
+    context 'stubbed api calls' do
+      before do
+        stub_request(:post, Rails.application.secrets.slack_webhook)
+        allow(subject).to receive(:send_message).and_call_original
+      end
+
+      it 'includes a link' do
+        subject.call
+        expect(subject).to have_received(:send_message).with(/#{link.url}/)
+      end
+    end
   end
 end
