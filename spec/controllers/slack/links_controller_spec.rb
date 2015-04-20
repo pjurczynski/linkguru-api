56bd3dce9afb2922ea6ea8ba8cb3abbefe04fbab
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-04-20 10:44:33 +0200

    additional slack links controller specs

--- spec/controllers/slack/links_controller_spec.rb
+++ spec/controllers/slack/links_controller_spec.rb
@@ -2,24 +2,103 @@ require 'rails_helper'
 
 module Slack
   describe LinksController do
-    let(:params) do
-      {
-        "token"=>"2H4D65bKXHlMIdrQOdoxCvPG",
-        "team_id"=>"T0251EZGA",
-        "team_domain"=>"netguru",
-        "service_id"=>"4484363840",
-        "channel_id"=>"C02KPPDS7",
-        "channel_name"=>"sandbox",
-        "timestamp"=>"1429281463.000014",
-        "user_id"=>"U02J1P00M",
-        "user_name"=>"szujekdobrarada",
-        "text"=>"add link http://something.com",
-        "trigger_word"=>"add link",
-      }
+    include_context 'szujek is a member'
+
+    let!(:outbound_request) do
+      stub_request(:post, Rails.application.secrets.slack_webhook)
     end
 
-    describe '#create' do
-      it 'should create a link'
+    describe "#create" do
+      let(:params) do
+        build(
+          :slack_api_request,
+          user_name: slack_szujek['name'],
+          text: "add link <http://something.co> some description #tag #tag2",
+          trigger_word: "add link"
+        )
+      end
+
+      subject { post :create, params }
+      it { expect{ subject }.to change{ Link.count }.by(1) }
+      it { expect{ subject }.to change{ Link.first.url }.to('http://something.co') }
+      it { expect{ subject }.to change{ Link.first.tags.count }.to(2) }
+      it { expect{ subject }.to change{ Link.first.tags.pluck(:name) }.to(%w[tag2 tag]) }
+      it "returns success message in json" do
+        expect(subject.body)
+          .to eq({ text: I18n.t('slack.links.create_success') }.to_json)
+      end
+
+      context "Link couldn't be saved" do
+        let(:params) do
+          build(
+            :slack_api_request,
+            user_name: slack_szujek['name'],
+            text: "add link but no link!",
+            trigger_word: "add link"
+          )
+        end
+
+        it "returns failing message in json" do
+          expect(subject.body)
+            .to eq({ text: I18n.t('slack.links.create_failed') }.to_json)
+        end
+      end
+    end
+
+    describe "#last_upvote" do
+      let!(:old_link) { create(:link) }
+      let!(:fresh_link) { create(:link) }
+      let(:params) do
+        build(
+          :slack_api_request,
+          "user_name" => slack_szujek['name'],
+          "text" => "upvote",
+          "trigger_word" => "upvote",
+        )
+      end
+      subject { post :last_upvote, params }
+
+      it "upvotes newly added link" do
+        expect{ subject }.to change{ fresh_link.votes_for.count }.by(1)
+      end
+
+      it "other links doesn't change" do
+        expect{ subject }.not_to change{ old_link.votes_for.count }
+      end
+
+      it "sends a message to slack channel" do
+        subject
+        expect(outbound_request)
+          .to have_requested(:post, Rails.application.secrets.slack_webhook)
+      end
+    end
+
+    describe "#last_downvote" do
+      let!(:old_link) { create(:link) }
+      let!(:fresh_link) { create(:link) }
+      let(:params) do
+        build(
+          :slack_api_request,
+          "user_name" => slack_szujek['name'],
+          "text" => "downvote",
+          "trigger_word" => "downvote",
+        )
+      end
+      subject { post :last_downvote, params }
+
+      it "downvotes newly added link" do
+        expect{ subject }.to change{ fresh_link.get_downvotes.count }.by(1)
+      end
+
+      it "other links doesn't change" do
+        expect{ subject }.not_to change{ old_link.get_downvotes.count }
+      end
+
+      it "sends a message to slack channel" do
+        subject
+        expect(outbound_request)
+          .to have_requested(:post, Rails.application.secrets.slack_webhook)
+      end
     end
   end
 end
