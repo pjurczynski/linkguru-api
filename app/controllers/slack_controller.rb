471df63084080927d02e74a9ea41213402a76bff
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-04-20 10:40:30 +0200

    use secrets to store trigger words

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -6,15 +6,17 @@ class SlackController < ActionController::API
   def current_slack_user
     return @current_slack_user if @current_slack_user.present?
     users = Slack::API::UserList.new.call
-    slack_user = users.find { |user| user['name'] == params['user_name'] }
+    slack_user = users.find { |user| user['name'] == params[:user_name] }
     @current_slack_user = User.find_by_email(slack_user['profile']['email'])
   end
 
   def token_valid?
-    params[:token] == Rails.application.secrets.slack_outgoing_token
+    params.fetch(:token) == Rails.application.secrets.slack_outgoing_token
   end
 
   def trigger_word_valid?
-    %w[add\ link linkguru].include? params[:trigger_word]
+    Rails.application.secrets.trigger_words.fetch('list').include?(
+      params[:trigger_word]
+    )
   end
 end
