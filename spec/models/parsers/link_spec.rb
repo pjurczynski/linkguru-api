50b47a136f0bd63b7b0a595e5ed24d68c190aaa2
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-04-20 10:43:32 +0200

    additional link specs

--- spec/models/parsers/link_spec.rb
+++ spec/models/parsers/link_spec.rb
@@ -7,6 +7,22 @@ module Parsers
     it { expect(subject.description).to eq 'some description' }
     it { expect(subject.tag_list).to match_array [['tag'], ['<#tag2>']] }
 
+    context 'with two normal tags' do
+      let(:body) { 'add link <http://something.co> some description #tag #tag2' }
+
+      it { expect(subject.url).to eq 'http://something.co' }
+      it { expect(subject.description).to eq 'some description' }
+      it { expect(subject.tag_list).to match_array [['tag'], ['tag2']] }
+    end
+
+    context 'with two special tags' do
+      let(:body) { 'add link <http://something.co> some description <#tag> <#tag2>' }
+
+      it { expect(subject.url).to eq 'http://something.co' }
+      it { expect(subject.description).to eq 'some description' }
+      it { expect(subject.tag_list).to match_array [['<#tag>'], ['<#tag2>']] }
+    end
+
     context 'no description has been passed' do
       let(:body) { 'add link <http://something.co> #tag <#tag2>' }
 
