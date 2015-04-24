d4c2f60518d7431d64a9eb0a4910a14da87115a0
Rafał Gawlik <rafal.maly@gmail.com>
2015-04-24 19:20:53 +0200

    use subject here as before

--- spec/controllers/slack/links_controller_spec.rb
+++ spec/controllers/slack/links_controller_spec.rb
@@ -19,7 +19,7 @@ module Slack
       end
 
       subject { post :create, params }
-      it { expect{ Link.count }.to change{ Link.count }.by(1) }
+      it { expect{ subject }.to change{ Link.count }.by(1) }
       xit { expect{ subject }.to change{ Link.first.url }.to('http://something.co') }
       xit { expect{ subject }.to change{ Link.first.tags.count }.to(2) }
       xit { expect{ subject }.to change{ Link.first.tags.pluck(:name) }.to(%w[tag2 tag]) }
